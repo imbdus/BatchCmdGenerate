2134515757d912be6db0548cdeff53531b44eb8c
bdus <rovingthrough@163.com>
2017-09-01 15:45:34 +0800

    version 1.0
    可以用的版本

---
+++ Batch/Batch/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Batch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tb_fileextension = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tb_re = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(822, 334);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem5});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(822, 25);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.tb_fileextension});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 21);
+            this.toolStripMenuItem1.Text = "load";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(181, 22);
+            this.toolStripMenuItem2.Text = "+ Load Subfiles";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(181, 22);
+            this.toolStripMenuItem3.Text = "+ Load SubFolder";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(181, 22);
+            this.toolStripMenuItem4.Text = "+ Load One";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // tb_fileextension
+            // 
+            this.tb_fileextension.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_fileextension.Name = "tb_fileextension";
+            this.tb_fileextension.Size = new System.Drawing.Size(60, 23);
+            this.tb_fileextension.Text = "*.jpg";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tb_re,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7});
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(36, 21);
+            this.toolStripMenuItem5.Text = "do";
+            // 
+            // tb_re
+            // 
+            this.tb_re.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_re.Name = "tb_re";
+            this.tb_re.Size = new System.Drawing.Size(300, 23);
+            this.tb_re.Text = "scissors.exe -p $1 -i $2 $3";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(360, 22);
+            this.toolStripMenuItem6.Text = "+ RegularExpression Test";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(360, 22);
+            this.toolStripMenuItem7.Text = "+ Generate";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 359);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Batch cmd Generater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox tb_fileextension;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripTextBox tb_re;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Batch/Batch/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Batch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string path;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            //Add column header
+            listView1.Columns.Add("$1", 300);
+            listView1.Columns.Add("$2", 100);
+            listView1.Columns.Add("$3", 400);
+
+            //tooltops loading
+            tooltips(sender, e);
+        }
+
+        private void tooltips(object sender, EventArgs e)
+        {
+            //this.toolTip1.SetToolTip(this.bt_loadsub, "选择一个文件，获取文件夹下所有的相同类文件");
+        }
+
+
+        // bt_loadsub
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Chose folder";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK || openFileDialog1.ShowDialog() == DialogResult.Yes)
+            {
+                path = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+
+                //获取目录下文件名
+                DirectoryInfo folder = new DirectoryInfo(path);
+                foreach (FileInfo item in folder.GetFiles("*" + Path.GetExtension(openFileDialog1.FileName)))
+                {                   
+                    string[] arr = {
+                        item.Directory.FullName,
+                        item.Name,
+                        item.FullName
+                    };
+                    listView1.Items.Add(new ListViewItem(arr));
+                }
+            }
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK || folderBrowserDialog1.ShowDialog() == DialogResult.Yes)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+
+                //获取所有的子目录
+                DirectoryInfo di = new DirectoryInfo(path);//https://msdn.microsoft.com/en-us/library/s7xk2b58(v=vs.110).aspx
+                DirectoryInfo[] diArr = di.GetDirectories("*.*", System.IO.SearchOption.AllDirectories);//https://docs.microsoft.com/zh-cn/dotnet/csharp/programming-guide/file-system/how-to-iterate-through-a-directory-tree
+
+                foreach (DirectoryInfo dir in diArr)
+                {
+                    //MessageBox.Show("Test" + dir.Name + " "+ dir.Root + " " + dir.FullName);
+                    //dir.FullName 就是目录树下的所有子目录的文件
+
+                    foreach (FileInfo item in dir.GetFiles(tb_fileextension.Text))
+                    {
+                        string[] arr = {
+                            item.Directory.FullName,
+                            item.Name,
+                            item.FullName//mc[0].Value
+                        };
+                        listView1.Items.Add(new ListViewItem(arr));
+                    }//foreach
+                }//foreach
+            }//fi
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Chose one";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo item = new FileInfo(openFileDialog1.FileName);
+
+                string[] arr = {
+                        item.Directory.FullName,
+                        item.Name,
+                        item.FullName
+                    };
+                listView1.Items.Add(new ListViewItem(arr));
+            }
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items != null)
+            {
+
+                string[] nums = {
+                @listView1.Items[0].SubItems[0].Text,
+                @listView1.Items[0].SubItems[1].Text,
+                @listView1.Items[0].SubItems[2].Text
+                //@listView1.SelectedItems[0].SubItems[2].Text
+             };
+
+
+                string[] patterns =  {
+                    @"\p{Sc}1",@"\p{Sc}2",@"\p{Sc}3"
+            };
+                //https://msdn.microsoft.com/zh-cn/library/e7f5w83z(v=vs.110).aspx
+                //https://msdn.microsoft.com/zh-cn/library/ewy2t5e0(v=vs.110).aspx
+
+                string ans = @tb_re.Text;
+
+                for (int i = 0; i < patterns.Length; i++)
+                {
+                    //MessageBox.Show("for: " + input_text+"  "+  patterns[i] + " "+ nums[i]);
+                    ans = @Regex.Replace(@ans, @patterns[i], @nums[i]);
+                }
+
+                MessageBox.Show(@ans);
+
+
+                //MatchCollection mc = Regex.Matches(item.Name, pattern);
+                //MessageBox.Show(mc[0].Value);
+            }
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items != null)
+            {
+                //正则替换的结果
+                List<string> ans_l = new List<string>();
+
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    string ans = @tb_re.Text;
+                    for (int i = 0; i < item.SubItems.Count; i++)
+                    {
+                        //进行正则替换 通过 listView1.Items[0].SubItems.Count 次替换 替换所有的参数 生成ans
+                        ans = @Regex.Replace(@ans, (@"\p{Sc}" + (i + 1)), @item.SubItems[i].Text);
+                    }
+                    ans_l.Add(@ans);     //MessageBox.Show(ans);
+                }//foreach
+
+                //save ans_l into text file
+                SaveFileDialog svd = new SaveFileDialog();
+                svd.Title = "Save commands to...";
+                if (svd.ShowDialog() == DialogResult.OK)
+                {
+                    if (svd.CheckPathExists)
+                    {
+                        File.WriteAllLines(svd.FileName, ans_l.ToArray());
+                    }
+
+                    //打开文件夹
+                    System.Diagnostics.Process.Start("notepad.exe", svd.FileName);
+                }
+
+            }//fi
+        }
+    }
+}
